1130d42a7864b2d6f4f7b2ed5a59c97d8015d454
FrankMcLowell <46972810+FrankMcLowell@users.noreply.github.com>
2019-07-04 10:13:08 -0300

    Add files via upload

---
+++ CMDAv0ider.bat
@@ -0,0 +1,30 @@
+
+::========================================================
+::
+:: CMDAv0ider - a exploit shell
+:: Created by Frank McLowell (fwesk3r)
+:: http://github.io/MrMcLowell
+::
+::========================================================
+
+echo off
+
+cls
+
+echo CMDAv0ider v0.1 (Alpha)
+echo - a simple Triple Alliance tool - 
+
+echo.
+
+echo ----------------------------------
+:a
+
+echo.
+
+set /p command="%cd%: "
+
+echo.
+
+%command%
+
+goto a
